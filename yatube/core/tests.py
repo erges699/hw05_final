b585412cb876fa4d67611707236e9c9520287264
Serge Balyaba <erges699@gmail.com>
2022-03-29 18:48:54 +0300

    FIN_01

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -5,7 +5,6 @@ from http import HTTPStatus
 
 class ViewTestClass(TestCase):
     def test_error_page(self):
-        self.template = 'core/404 page_not_found.html'
         response = self.client.get('/nonexist-page/')
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        self.assertTemplateUsed(response, self.template)
+        self.assertTemplateUsed(response, 'core/404 page_not_found.html')
--- yatube/core/views.py
+++ yatube/core/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404 page_not_found.html', {'path': request.path}, status=404)
+    return render(request, 'core/404 page_not_found.html',
+                  {'path': request.path}, status=404)
 
 
 def server_error(request):
-    return render(request, 'core/500.html', status=500)
+    return render(request, 'core/500 server_error.html', status=500)
 
 
 def permission_denied(request, exception):
